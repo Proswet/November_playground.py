d7e23f76aea053a67b9db15cc57171ea14d897d6
Proswet <proswet472@gmaol.com>
2022-12-03 19:40:18 +0300

    November_2.py

--- November_1.py
+++ November_1.py
@@ -17,15 +17,5 @@ from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', None)
-files = glob.glob("November\submission_files\*.csv")
-df = pd.read_csv("November\labels_train.csv")
-suf = 0
-for file_name in files:
-    file = file_name[-16:]
-    df_pred = pd.read_csv(f"November\submission_files\{file}")
-    df = df.join(df_pred["pred"], on="id", rsuffix=suf, how="outer")
-    
-    suf += 1
-    print(suf)
+df = pd.read_csv("November/Train.csv")
 print(df)
-df.to_csv("November/Train.csv")
